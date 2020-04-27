3b6b488f34682d88747db17929ed03c40dc220af
sirgallo <nickgallo97@gmail.com>
2020-04-27 11:11:27 -0400

    Slight change

--- schemefront/src/App.vue
+++ schemefront/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <b-navbar type="light" variant="light">
-      <b-navbar-brand>Scheme</b-navbar-brand>
+      <b-navbar-brand>Sight</b-navbar-brand>
       <b-navbar-nav>
         <b-link class="mr-3" to="/">Analyze</b-link>
         <b-link to="/dashboard">Dashboard</b-link>
