7befbd7b402c100ffcb3e00e8e00782f1e76e93d
Nick Gallo <nickgallo97@gmail.com>
2020-11-18 16:09:23 -0500

    update

--- installdockeranddeploy.sh
+++ installdockeranddeploy.sh
@@ -9,5 +9,3 @@ sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose up --build
