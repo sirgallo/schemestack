68749a6ca5672a88c15ce4e4ac133cd4b57e81d6
Nick Gallo <nickgallo97@gmail.com>
2020-11-19 12:46:31 -0500

    test

--- deployAmazonLinux2.sh
+++ deployAmazonLinux2.sh
@@ -17,8 +17,8 @@ else
   sudo usermod -a -G docker ec2-user
   sudo systemctl enable docker
 
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+  sudo chmod +x /usr/bin/docker-compose
 
   sudo reboot
 fi
