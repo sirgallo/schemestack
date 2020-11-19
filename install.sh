56abed6c5cabe9ce264407b60c5235a2cc18aaaa
Nick Gallo <nickgallo97@gmail.com>
2020-11-19 15:20:25 -0500

    test again

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+schemedir=$(pwd)
+deploypath = "$schemedir/deployAmazonLinux2.sh"
 sudo chmod u+x deployAmazonLinux2.sh
-sudo systemctl enable run-at-startup.service
-sudo bash deployAmazonLinux2.sh
+echo $deploypath
+
+cd /
+
+sudo systemctl enable $deploypath
+sudo bash $deploypath
