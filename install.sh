e64f6f6b41f2b63078703959e374e4d4bc770f26
Nick Gallo <nickgallo97@gmail.com>
2020-11-19 16:13:09 -0500

    test

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo "Get current application path.."
 schemedir=$(pwd)
-deploypath = "$schemedir/deployAmazonLinux2.sh"
+echo $schemedir
+bashscript="/deployAmazonLinux2.sh"
+deploypath=$schemedir$bascript
+echo "Give executable permissions to deployment script"
 sudo chmod u+x deployAmazonLinux2.sh
+echo "Path of deployment"
 echo $deploypath
 
 cd /
 
+echo "Enable application on application"
 sudo systemctl enable $deploypath
 sudo bash $deploypath
