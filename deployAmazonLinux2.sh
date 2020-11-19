3a48895fc561c687bb6dbcdf9801bb4d6789f90f
Nick Gallo <nickgallo97@gmail.com>
2020-11-19 12:36:46 -0500

    test

--- deployAmazonLinux2.sh
+++ deployAmazonLinux2.sh
@@ -20,4 +20,5 @@ else
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 
+  sudo reboot
 fi
